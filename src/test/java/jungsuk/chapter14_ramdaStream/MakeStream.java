de1256cb2c4baca3c32bc1c616a6a97d318c75b4
sinyoung yun <54181528+yenow@users.noreply.github.com>
2021-09-01 08:55:39 +0900

    스트림 생성

--- src/test/java/jungsuk/chapter14_ramdaStream/MakeStream.java
+++ src/test/java/jungsuk/chapter14_ramdaStream/MakeStream.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 public class MakeStream {
@@ -42,8 +44,22 @@ public class MakeStream {
 	2. 스트림은 일회용이다.
 
 	3. 스트림은 작업을 내부 반복으로 처리한다.
-	 - 스트림을 이용한 작업이 간결할 수 있는 비결중에 하나가 '내부반복'이다. 내부 반복이라는것은 반복문을을
-	/
+	 - 스트림을 이용한 작업이 간결할 수 있는 비결중에 하나가 '내부반복'이다. 내부 반복이라는것은 반복문을 메서드의 내부에 숨겼다는 것을 의미한다
+	 - forEach() 스트림에 정의된 메서드 중의 하나로 매개변수에 대입된 람다식을 데이터 소스의 모든 요소에 적용한다.
+	 
+	4. 지연된 연산
+	 - 최종연산이 수행되기 전까지는 중간 연산이 수행되지 않는다.
+	 - distinct()나 sort()같은 중간 연산을 호출해도 즉각적인 연산이 수행되지 않는다. 
+	 
+	 5. Stream<Integer>와 IntStream 
+	 - 요소의 타입이 T인 스트림은 Stream<T>이지만, 오토박싱&언박싱으로 인한 비효율을 줄이기 위해 데이터 소스를 기본형으로 다루는
+	   IntStream, LognStream 등이 제공된다.(더 효율적)
+	   
+	 6. 병렬 스트림
+	  - 내부적으로 Java에서 fork & join 프레임웍을 이용해서 자동적으로 연산을 병렬로 수행
+	  - parallel() 메서드 호출
+	  -  
+     */
     @Test
     public void l16_스트림_특징() {
         String[] strArr = {"1","2","3","4","5"};
@@ -56,6 +72,63 @@ public class MakeStream {
         // 출력
         strStream1.sorted().forEach(System.out::println);  // 아직 소스가 정렬되는건 아니라고한다.
         strStream2.sorted().forEach(System.out::println);
+
+        // 병렬 스트림 예시
+        int sum = Arrays.stream(strArr).parallel().mapToInt(s -> s.length()).sum();
+        System.out.println("sum : " + sum);
+    }
+
+    /*
+    @Title : l17_스트림만들기_컬렉션
+	@Content
+	- 스트림의 소스가 될 수 있는 대상은 배열, 컬렉션, 임의의 수 등 다양하다.
+     */
+    @Test
+    public void l17_스트림만들기_컬렉션() {
+
+        // 컬렉션의 최고 조상에 stream() 메서드가 정의되어 있다.  ex) Stream<T> Collection.stream()
+        List<Integer> list = Arrays.asList(1,2,3,4,5);
+        Stream<Integer> intStream = list.stream();
+
+        // forEach()는 지정된 작업을 스트림의 모든 요소에 대해 수행한다.
+        intStream.forEach(System.out::println);   //  스트림의 모든 요소 출력
+        // intStream.forEach(System.out::println);   //  에러, 스트림이 닫혔다.
+    }
+
+    /*
+    @Title : l18_스트림만들기_배열
+	@Content
+     */
+    @Test
+    public void l18_스트림만들기_배열() {
+
+        // 배열스트림 만드는 방법
+        Stream<String> strStream1 = Stream.of("a","b","c");
+        Stream<String> strStream2 = Stream.of(new String[] {"a","b","c"});
+        Stream<String> strStream3 = Arrays.stream(new String[] {"a","b","c"});
+        Stream<String> strStream4 = Arrays.stream(new String[] {"a","b","c"},0,3);
+    }
+
+    /*
+    @Title : l19_스트림만들기_난수
+	@Content
+	난수의 범위
+	 - Integer.MIN_VALUE <= ints() <= Integer.MAX_VALUE
+	 - Long.MIN_VALUE <= longs() <= Long.MAX_VALUE
+	 - 0.0 <= doubles() <= 1.0
+     */
+    @Test
+    public void l19_스트림만들기_난수() {
+
+        // Random 클래스에 난수로 이루어진 스트림을 반환하는 메서드
+        IntStream intStream = new Random().ints();  // 매개변수가 없으면 무한 스트림
+        LongStream longStream = new Random().longs();  // 매개변수가 없으면 무한 스트림
+        DoubleStream doubleStream = new Random().doubles();  // 매개변수가 없으면 무한 스트림
+
+        // 유한스트림으로 변경
+        IntStream intStream2 = new Random().ints().limit(5);  // 유한스트림
+        LongStream longStream2 = new Random().longs().limit(5);
+        DoubleStream doubleStream2 = new Random().doubles().limit(5);
     }
 
     /* 스트림의 소스가 될 수 있는 대상은 배열, 컬렉션, 임의의 수 등 다양 */
